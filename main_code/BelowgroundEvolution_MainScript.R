f504372e03fdfb63cf798ca59430bf96a0da1909
MVahsen <mlvahsen@gmail.com>
2021-09-22 13:16:13 -0500

    fit mono and cs models

--- main_code/BelowgroundEvolution_MainScript.R
+++ main_code/BelowgroundEvolution_MainScript.R
@@ -4,11 +4,98 @@
 # main_code folder
 
 ## Load libraries ####
-library(tidyverse); library(ggmcmc); library(rjags)
+library(tidyverse); library(ggmcmc); library(rjags); library(lme4)
 
 ## Read in data ####
 
 # Pot-level trait data for (generalized) linear mixed models
 pot_traits <- read_csv("data/BelowgroundEvolution_PotLevel.csv")
 
-# Source JAGS functions to fit (generalized) linear mixed models
+## Data formatting ####
+# Set appropriate factors
+pot_traits %>% 
+  mutate(pot = factor(pot),
+         frame = factor(frame),
+         diversity = factor(diversity),
+         age = factor(age),
+         provenance = factor(provenance),
+         genotype = factor(genotype)) -> pot_traits
+# Subset data into all monoculture data and all polyculture data
+pot_traits %>% 
+  filter(diversity == "mono") -> mono_traits
+pot_traits %>% 
+  filter(diversity == "poly") -> poly_traits
+
+
+## Source JAGS functions to fit (generalized) linear mixed models ####
+source("main_code/jags_fxns.R")
+
+## Create a model template that JAGS will use the model matrix for ####
+model_template_mono <- lmer(agb ~ ln_depth + ic_weight + frame +
+                         (1|genotype), data = mono_traits)
+
+## Set MCMC specifications ####
+n.iter <- 10000
+n.adapt <- 1000
+thin <- 3
+
+## Fit all basic trait models ####
+
+## aboveground biomass
+agb_out <- run_models(mono_traits, "agb", model_template_mono, diag_plot = F)
+## stem density
+density_out <- run_models(mono_traits, "density", model_template_mono, diag_plot = F)
+# Fails to set monitor for sigma.res because there is no residual variation in
+# Poisson regression (aka this warning is ok and expected)
+## stem height
+height_out <- run_models(mono_traits, "mean_tot_height", model_template_mono, diag_plot = F)
+## stem width
+width_out <- run_models(mono_traits, "mean_mid_width", model_template_mono, diag_plot = F)
+## belowground biomass
+bgb_out <- run_models(mono_traits, "bgb", model_template_mono, diag_plot = F)
+## root-to-shoot ratio
+rs_out <- run_models(mono_traits, "rs", model_template_mono, diag_plot = F)
+## beta (belowground biomass distribution parameter)
+beta_out <- run_models(mono_traits, "beta", model_template_mono, diag_plot = F)
+
+# Save as R data file objects to use later for plotting and subsequent analyses
+saveRDS(agb_out, "outputs/agb_monomodel.rds")
+saveRDS(rs_out, "outputs/rs_monomodel.rds")
+saveRDS(beta_out, "outputs/beta_monomodel.rds")
+saveRDS(width_out, "outputs/width_monomodel.rds")
+saveRDS(height_out, "outputs/height_monomodel.rds")
+saveRDS(rs_out, "outputs/rs_monomodel.rds")
+saveRDS(bgb_out, "outputs/bgb_monomodel.rds")
+
+## Fit all trait models for age + provenance additive effects ####
+
+# Subset data to just Corn Island and Sellman Creek because those provenances
+# have multiple reps within each age cohort (only drops 6 reps)
+mono_traits %>% 
+  filter(provenance %in% c("corn", "sellman")) -> cs_traits
+
+# Previous models fits showed non-significant age by provenance interactions for
+# all trait models. So here we fit all models with additive effects of age and
+# provenance 
+
+# Create new model template
+model_template_cs <- lmer(agb ~ ln_depth + ic_weight + frame +
+                            provenance + age + (1|genotype), data = cs_traits)
+
+## aboveground biomass
+agb_cs_out <- run_models(cs_traits, "agb", model_template_cs, diag_plot = F)
+## stem density
+density_cs_out <- run_models(cs_traits, "density", model_template_cs, diag_plot = F)
+# Fails to set monitor for sigma.res because there is no residual variation in
+# Poisson regression (aka this warning is ok and expected)
+## stem height
+height_cs_out <- run_models(cs_traits, "mean_tot_height", model_template_cs, diag_plot = F)
+## stem width
+width_cs_out <- run_models(cs_traits, "mean_mid_width", model_template_cs, diag_plot = F)
+## belowground biomass
+bgb_cs_out <- run_models(cs_traits, "bgb", model_template_cs, diag_plot = F)
+## root-to-shoot ratio
+rs_cs_out <- run_models(cs_traits, "rs", model_template_cs, diag_plot = F)
+## beta (belowground biomass distribution parameter)
+beta_cs_out <- run_models(cs_traits, "beta", model_template_cs, diag_plot = F)
+
--- main_code/jags_fxns.R
+++ main_code/jags_fxns.R
@@ -6,21 +6,31 @@
 sink("main_code/Gaussian_RI_Model.txt")
 cat("
 model{
-  alpha ~ dnorm(0, 0.001)
-  
-  for (k in 1:nbeta){
-    beta[k] ~ dnorm(0, 0.001) 
-  }
-  
-  sigma.res ~ dunif(0, 100) 
-  tau.res <- 1/(sigma.res^2)
-  
-  # Likelihood
-  for (i in 1:length(y)) {
-    mu[i] <- alpha + beta%*%X[i,] 
-    y[i] ~ dnorm(mu[i], tau.res) 
-  }
-}",fill = TRUE)
+    sigma.int ~ dunif(0, 100)
+    tau.int <- 1/(sigma.int^2)
+    
+    for (j in 1:nalpha) {
+    alpha[j] ~ dnorm(mu.alpha, tau.int) # Random by-subject deflections to the intercept
+    }
+    
+    mu.alpha ~ dnorm(0, 0.001) # The mean intercept
+    
+    for (k in 1:nbeta){
+    beta[k] ~ dnorm(0, 0.001) # Common slopes
+    }
+    
+    sigma.res ~ dunif(0, 100) # Residual standard deviation
+    tau.res <- 1/(sigma.res^2)
+    
+    # Likelihood
+    for (i in 1:length(y)) {
+    mu[i] <- alpha[genotype[i]] + beta%*%X[i,] 
+    y[i] ~ dnorm(mu[i], tau.res) # The actual (random) responses
+    
+
+    }
+    
+    }",fill = TRUE)
 sink()
 
 # Create Poisson random intercept model
@@ -54,6 +64,7 @@ sink()
 # Define get_samples functions (runs jags and extracts jags.samples and
 # dic.samples) for two types of models (Gaussian w/ genotype random intercept,
 # Poisson w/ genotype random intercept)
+
 get_samples <- function(model, data, trait){
   # Get model matrices from linear models
   M_all <- as.matrix(model.matrix(model)[ , -1])
@@ -70,10 +81,10 @@ get_samples <- function(model, data, trait){
   # Set up data structure
   nbeta <- ncol(M_all_sc)
   genotype <- as.numeric(droplevels(as.factor(data$genotype)))
-  y <- data[,trait]
+  y <- pull(data[,trait])
   nalpha <- length(unique(genotype))
   
-  data = list(
+  data_jags = list(
     y = y,
     X = M_all_sc,
     genotype = genotype,
@@ -100,13 +111,16 @@ get_samples <- function(model, data, trait){
   )
   
   # Fit JAGS model with gaussian likelihood and genotype as random intercept
-  jm_mono = jags.model("main_code/Gaussian_RI_Model.txt", data=data, 
+  jm_mono = jags.model("main_code/Gaussian_RI_Model.txt", data=data_jags, 
                        n.adapt = n.adapt, inits=inits, n.chains=length(inits))
   # Get DIC samples
   dic_mono = dic.samples(jm_mono, n.iter = n.iter)
   
   # Return both JAGS samples and DIC samples
   return(list(dic_mono = dic_mono, jm_mono = jm_mono))}
+
+
+
 get_samples_pois <- function(model, data){# Get model matrices from linear models
   
   M_all <- as.matrix(model.matrix(model)[ , -1])
@@ -121,7 +135,7 @@ get_samples_pois <- function(model, data){# Get model matrices from linear model
   y <- data$density
   nalpha <- length(unique(genotype))
   
-  data = list(
+  data_jags = list(
     y = y,
     X = M_all_sc,
     genotype = genotype,
@@ -145,7 +159,7 @@ get_samples_pois <- function(model, data){# Get model matrices from linear model
     )
   )
   
-  jm_mono = jags.model("main_code/Poisson_RI_Model.txt", data=data, 
+  jm_mono = jags.model("main_code/Poisson_RI_Model.txt", data=data_jags, 
                        n.adapt = n.adapt, inits=inits, n.chains=length(inits))
   
   dic_mono = dic.samples(jm_mono, n.iter = n.iter)
@@ -158,37 +172,12 @@ get_samples_pois <- function(model, data){# Get model matrices from linear model
 # This will run the model that will be either Gaussian or Poisson based on the
 # trait specified and will show convergence diagnostic plots if diag_plot is set
 # to TRUE
-run_models <- function(data, trait, diag_plot = F){
+run_models <- function(trait_data, trait, model_template, diag_plot = F){
   # Set up data structure
-  genotype <- as.numeric(droplevels(as.factor(data$genotype)))
-  y <- data[,trait]
-  nalpha <- length(unique(genotype))
-  
-  data = list(
-    y = y,
-    genotype = genotype,
-    nalpha = nalpha
-  )
-  
-  # Set initial values for chains (2)
-  inits = list(
-    list(
-      alpha = rep(0, nalpha),
-      sigma.res = 20,
-      sigma.int = 10,
-      mu.alpha = 8
-    ),
-    list(
-      alpha = rep(5, nalpha),
-      sigma.res = 2,
-      sigma.int = 12,
-      mu.alpha = 8
-    )
-  )
   if(trait == "density"){
-    jags_out <- get_samples_pois(model_template, data)
+    jags_out <- get_samples_pois(model_template, trait_data)
   }else{
-    jags_out <- get_samples(model_template, data, trait)
+    jags_out <- get_samples(model_template, trait_data, trait)
   }
   model_coda <- coda.samples(jags_out$jm_mono, variable.names = c("beta", "alpha","sigma.res","sigma.int", "mu.alpha"), n.iter=n.iter, thin = thin)  
   
