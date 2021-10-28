7a9b5e94f171490bf7d3946f0377e5a0d61104ae
mlvahsen <mlvahsen@gmail.com>
2021-10-28 11:42:23 -0500

    added percent change for difference in heights between cohorts

--- main_code/BelowgroundEvolution_MainScript.R
+++ main_code/BelowgroundEvolution_MainScript.R
@@ -296,6 +296,23 @@ mean((heights_sellman - heights_corn)/heights_corn) # 0.0297219
 # Calculate 95% credible interval percent increase from Corn to Sellman
 quantile((heights_sellman - heights_corn)/heights_corn, c(0.025, 0.975)) # -0.01462301  0.07517578   
 
+# Repeat the same process for age
+# Calculate predicted height for ancestral
+predicted_heights %>% 
+  dplyr::select(contains("age1")) %>% 
+  rowMeans() -> heights_anc
+
+# Calculate predicted height for modern
+predicted_heights %>% 
+  dplyr::select(contains("age2")) %>% 
+  rowMeans() -> heights_mod
+
+# Calculate mean percent increase from ancestral to modern
+mean((heights_mod - heights_anc)/heights_anc) # 0.003301437
+# Calculate 95% credible interval percent increase from ancestral to modern
+quantile((heights_mod - heights_anc)/heights_anc, c(0.025, 0.975)) # -0.03760141  0.04386239   
+
+
 
 ## Monoculture vs polyculture analysis ####
 
